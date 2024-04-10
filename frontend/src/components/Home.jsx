27f9e90e45602d226b01d91c6b5b25095cb3b0de
xelody <melodypxin@gmail.com>
2024-04-09 17:26:15 -0700

    added media queries for mobile users

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,70 +1,15 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { AuthContext } from './AuthContext';
-
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React from 'react';
 
 import Nav from './Nav'
 import Header from './Header';
 import '../styles/common.css';
+import iconImage from '../assets/icon.png';
 
 export default function Home() {
 
-    const { isLoggedIn, setIsLoggedIn, activeUsername, setActiveUsername } = useContext(AuthContext);
-
-    const [passwords, setPasswords] = useState([]);
     const isHomeActive = true;
     const isLogInActive = false;
     const isPasswordActive = false;
-
-    async function getAllPasswords() {
-        const response = await axios.get('/api/password/');
-        setPasswords(response.data);
-    }
-
-
-    const components = [];
-    for (let i = 0; i < passwords.length; i++) {
-        const pswd = passwords[i];
-        const passwordComponent = (<div>{pswd.urlAddress} - {pswd.password}</div>);
-        components.push(passwordComponent);
-    }
-
-    function setPokemonName(event) {
-        const pokemonName = event.target.value;
-        setPokemonInput({
-            ...pokemonInput,
-            /*
-            health: pokemonInput.health,
-            color: pokemonInput.color,
-            */
-            name: pokemonName
-        })
-    }
-
-    function setPokemonColor(event) {
-        const pokemonColor = event.target.value;
-        setPokemonInput({
-            ...pokemonInput,
-            /*
-            health: pokemonInput.health,
-            name: pokemonInput.name,
-            */
-            color: pokemonColor
-        })
-    }
-
-    function setPokemonHealth(event) {
-        const pokemonHealth = event.target.value;
-        setPokemonInput({
-            ...pokemonInput,
-            /*
-            health: pokemonInput.health,
-            color: pokemonInput.color,
-            */
-            health: pokemonHealth,
-        })
-    }
 
     return (
         <div>
@@ -72,10 +17,26 @@ export default function Home() {
                 isLogInActive={isLogInActive}
                 isPasswordActive={isPasswordActive} />
             <Header />
-            <div>
-                <div>{components}</div>
+            <div className='content-box box-border'>
+                <img src={iconImage} className='icon' alt='Icon' />
+                <div>
+                    <strong>Welcome to KeyKeeper!</strong><br /><br />
+                    KeyKeeper is a Password Manager App that provides a secure and convenient
+                    solution for individuals and businesses to track, generate,
+                    and share passwords. With the increasing importance of cybersecurity in
+                    today's digital landscape, KeyKeeper offers users peace of mind by enabling them to manage their
+                    passwords effectively. Users can securely store and organize their passwords
+                    for various accounts and services in one centralized location. KeyKeeper also offers
+                    the option to generate strong and unique passwords based on user-defined
+                    parameters, ensuring enhanced security for online accounts. Moreover, users can
+                    securely share passwords with trusted individuals with KeyKeeper accounts.
+                </div>
+            </div>
+            <div className='creator-box'>
+                <div>Site Creator: Peiyao Xin</div>
             </div>
         </div>
+
     )
 
 }
