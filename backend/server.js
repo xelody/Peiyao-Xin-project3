e3b8c680a5ce615e80eb3a7870a6edbbd9965ba8
xelody <melodypxin@gmail.com>
2024-04-13 21:43:42 -0700

    removed the unused script

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const helper = require('./apis/helper');
-const password = require('./apis/password')
-const users = require('./apis/user')
+const helper = require('./backend/apis/helper');
+const password = require('./backend/apis/password')
+const users = require('./backend/apis/user')
 const app = express();
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -26,7 +26,7 @@ app.use('/api/users/', users)
 
 
 
-let frontend_dir = path.join(__dirname, '..', 'frontend')
+let frontend_dir = path.join(__dirname, 'dist');
 
 app.use(express.static(frontend_dir));
 app.get('*', function (req, res) {
